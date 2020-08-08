954165c4aa47c47b7dc09e24870e12dc9df23090
debsdaniel <debs.foros@gmail.com>
2020-08-08 00:30:02 -0600

    07-017 Rutas Automaticas con Django Rest FrameWork y ModelViewSet

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
-from .views import prueba
+from rest_frameworks import routers
+
+from .views import prueba, DocumentoViewSet
+
+router = routers.DefaultRouter()
+router.register(r'docs',DocumentoViewSet)
+
 
 urlpatterns = [
-    path('prueba',prueba,name="prueba")
+    # path('prueba',prueba,name="prueba")
+    path('',include(router.urls))
 ]
