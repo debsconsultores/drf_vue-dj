9ff324e0ab2fc07dd9aa6d42e97989c7520e1036
debsdaniel <debs.foros@gmail.com>
2020-08-08 00:20:04 -0600

    07-017 Vista Model ViewSet para Modelo Documento

--- api/views.py
+++ api/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework import viewsets
+from .models import Documento
+from .serializer import DocumentoSerializer
+
+
 
 def prueba(request):
     return HttpResponse("Primera Vista del Curso")
 
+
+
+class DocumentoViewSet(viewsets.ModelViewSet):
+    queryset = Documento.objects.all().order_bye('id')
+    serializer_class = DocumentoSerializer
