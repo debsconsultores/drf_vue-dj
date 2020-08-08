1886e223dbe4de458b9607a98c4f09e9d676a79c
debsdaniel <debs.foros@gmail.com>
2020-08-08 00:03:16 -0600

    07-015 Modelo Documento para la APP Control de Documentos

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Documento(models.Model):
+    nombre = models.CharField(
+        max_length = 50,
+        null = False,
+        blank = False,
+        unique = True
+    )
+
+    expira = models.DateField()
+    alerta1y = models.BooleanField(default=True)
+    alerta6m = models.BooleanField(default=True)
+    alerta3m = models.BooleanField(default=True)
+    alerta1m = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    def save(self, **kwargs):
+        self.nombre = self.nombre.upper()
+        super(Documento,self).save()
+    
+    class Meta:
+        verbose_name_plural = "Documentos"
+
